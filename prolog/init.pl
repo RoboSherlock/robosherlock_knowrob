16e0535927c90e0ad69ab0e911490d99983f8dd1
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-05-14 14:51:19 +0200

    merged rs_prolog_interface

--- prolog/init.pl
+++ prolog/init.pl
@@ -1,9 +1,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % dependencies
 :- register_ros_package(knowrob_common).
-:- register_ros_package(knowrob_robosherlock).
+:- register_ros_package(knowrob_objects).
+:- register_ros_package(knowrob_vis).
+
+:- register_ros_package(robosherlock_knowrob).
 
 :- use_module(library(rs_plan_pipeline)).
+:- use_module(library(rs_query_interface)).
+:- use_module(library(rs_similar_objects)).
+:- use_foreign_library('rs_prologrulescpp.so').
+
+
+%:- owl_parser:owl_parse('package://iai_kitchen/owl/iai-kitchen-objects.owl').
+:- owl_parser:owl_parse('package://rs_prolog_interface/owl/rs_iai_objects.owl').
+
+:- rdf_db:rdf_register_prefix(kitchen, 'http://knowrob.org/kb/iai-kitchen.owl#', [keep(true)]).
+%:- rdf_db:rdf_register_prefix(rs_objects, 'http://knowrob.org/kb/rs_objects.owl#', [keep(true)]).
 
 :- owl_parser:owl_parse('package://knowrob_robosherlock/owl/rs_components.owl').
 :- rdf_db:rdf_register_prefix(rs_components, 'http://knowrob.org/kb/rs_components.owl#', [keep(true)]).
