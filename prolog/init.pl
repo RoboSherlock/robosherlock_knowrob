82f6f6841978eb471ef94097a5e239dcb559a8be
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-07-27 21:24:01 +0200

    started new module for planning with values

--- prolog/init.pl
+++ prolog/init.pl
@@ -6,17 +6,18 @@
 :- register_ros_package(knowrob_srdl).
 :- register_ros_package(robosherlock_knowrob).
 
-:- use_module(library(rs_plan_pipeline)).
-:- use_module(library(rs_query_interface)).
-:- use_module(library(rs_similar_objects)).
-
 :- owl_parser:owl_parse('package://iai_kitchen/owl/iai-kitchen-objects.owl').
-%:- owl_parser:owl_parse('package://robosherlock_knowrob/owl/rs_iai_objects.owl').
-
 :- rdf_db:rdf_register_prefix(kitchen, 'http://knowrob.org/kb/iai-kitchen.owl#', [keep(true)]).
-%:- rdf_db:rdf_register_prefix(rs_objects, 'http://knowrob.org/kb/rs_objects.owl#', [keep(true)]).
 
 :- owl_parser:owl_parse('package://robosherlock_knowrob/owl/rs_components.owl').
 :- rdf_db:rdf_register_prefix(rs_components, 'http://knowrob.org/kb/rs_components.owl#', [keep(true)]).
+
+
+% :- use_module(library(rs_plan_pipeline)).
+:- use_module(library(rs_query_interface)).
+:- use_module(library(rs_query_reasoning)).
+:- use_module(library(rs_similar_objects)).
+
+
 
 
